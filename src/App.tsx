2a59710b18659b35ac0c81b56f0274304ec72824
Nghiatran2603 <107810710+NghiaTV2603@users.noreply.github.com>
2023-04-29 14:57:25 +0700

    setup theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRoutes } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import routes from './app/routes';
+import { ColorModeContext, useMode } from './app/theme';
 
 function App() {
    const routing = useRoutes(routes);
+   const [theme, colorMode] = useMode();
    // eslint-disable-next-line react/jsx-no-useless-fragment
-   return <>{routing}</>;
+   return (
+      <ColorModeContext.Provider value={colorMode}>
+         <ThemeProvider theme={theme}>
+            <CssBaseline />
+            {routing}
+         </ThemeProvider>
+      </ColorModeContext.Provider>
+   );
 }
 
 export default App;
